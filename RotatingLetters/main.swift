c1ed768b929e8638f52daa327b15cfd34ecd9f86
Rubin Zach <zrubin@lcs-mba3136.local>
2018-04-06 14:12:51 -0400

    WIP

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -7,11 +7,39 @@
 //
 
 import Foundation
+var acceptableCharacters = 0
 
 // Get the user input
 var rawInput = readLine()
+
+//Make the user input not optional
+guard let input = rawInput else {
+    //ERROR
+    exit(9)
+}
+
+if input.characters.count < 1 || input.characters.count > 30 {
+    exit(9)
+}
+
+for individualCharacter in input {
+    
+    
+    // Categorize the character
+    switch individualCharacter {
+    case "I", "O", "S", "H", "Z", "X", "N":
+        acceptableCharacters += 1
+    default:
+        break
+    }
+}
 
 // Print out the input provided
 print("You said:")
 print(rawInput)
 
+if acceptableCharacters == individualCharacters.count {
+    print("YES")
+} else {
+    print("NO")
+}
