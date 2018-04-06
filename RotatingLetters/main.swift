bfe030d07f2a8c80744d17eed77a9ab71f516d68
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-04-06 09:58:55 -0400

    Initial Commit

---
+++ RotatingLetters/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  RotatingLetters
+//
+//  Created by Gordon, Russell on 2018-04-06.
+//  Copyright © 2018 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+// Get the user input
+var rawInput = readLine()
+
+// Print out the input provided
+print("You said:")
+print(rawInput)
+
